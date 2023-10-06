d771396c637c69a887286ab550a3ac04ca32af73
Marjan Tomev <marjan@tie-international.com>
2023-10-06 10:50:54 +0200

    added start script

--- server.js
+++ server.js
@@ -40,6 +40,11 @@ app.post('/sendEmail', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
